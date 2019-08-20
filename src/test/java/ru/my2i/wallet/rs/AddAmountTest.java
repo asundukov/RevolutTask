bb4a1c4e05b7bacb4ea1306ce5c36c29c3121950
AndreySundukov <lulonik@mail.ru>
2019-08-20 14:40:21 +0300

    more dry

--- src/test/java/ru/my2i/wallet/rs/AddAmountTest.java
+++ src/test/java/ru/my2i/wallet/rs/AddAmountTest.java
@@ -4,16 +4,12 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.my2i.wallet.rs.dto.UserDto;
 
-import java.util.UUID;
-
 public class AddAmountTest extends AbstractRsTest {
     private UserDto currentUserId;
 
     @BeforeMethod
     public void beforeTest() {
-        this.currentUserId = new UserDto();
-        this.currentUserId.service = "testAddAmount";
-        this.currentUserId.id = UUID.randomUUID().toString();
+        this.currentUserId = generateNewUser("testAddAmount");
     }
 
 
